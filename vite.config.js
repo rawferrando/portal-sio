3ff41e643e3386e0c165d1a26cc273b03af8d547
Raul Ferrando <rferrando@icm.csic.es>
2026-03-09 10:29:17 +0100

    Añadida la ruta base de Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // ESTA ES LA LÍNEA MÁGICA QUE NECESITAMOS AÑADIR:
+  base: '/portal-sio/',
+  
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
