56949684c835e4c4a55dbd2e698876f69d16b11f
Raul Ferrando <rferrando@icm.csic.es>
2026-05-26 16:34:08 +0200

    Configurado despliegue en carpeta docs

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // ESTA ES LA LÍNEA MÁGICA QUE NECESITAMOS AÑADIR:
   base: '/portal-sio/',
-  
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // ESTA ES LA CLAVE:
+  build: {
+    outDir: 'docs'
   }
 })
