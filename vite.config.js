379209de5d3df60e785bac39399a5f0099a2882d
Raul Ferrando <rferrando@icm.csic.es>
2026-05-27 08:16:35 +0200

    Fix crítico: cambiar outDir a docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/portal-sio/', // <-- ESTO ES CLAVE
-  plugins: [vue()]
+  base: '/portal-sio/', 
+  plugins: [vue()],
+  build: {
+    outDir: 'docs', // <-- ¡Esto obliga a Vite a usar la carpeta de GitHub Pages!
+    emptyOutDir: true // <-- Esto limpia la carpeta antes de construir para evitar basura vieja
+  }
 })
